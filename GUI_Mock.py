4b3f6cc9f747f6e2f030c1a6ed59df92ef0127c8
Ellyonia <ckeith@smu.edu>
2018-12-04 22:42:42 -0600

    Updated the look of the GUI (No connectivity yet)

--- GUI_Mock.py
+++ GUI_Mock.py
@@ -9,6 +9,8 @@ from PyQt5.QtCore import pyqtSlot
 class Ui_Form(object):
     
     def readFile(self, fileName):
+        if (fileName == ''):
+            return " "
         file = open(fileName, 'r')
         return file.readline()
  
@@ -20,8 +22,8 @@ class Ui_Form(object):
         # Create Label for Config File Input
         self.textLabel = QLabel(self)
         self.textLabel.setText("Please Enter the Config File Name: ")
-        self.textLabel.resize(280,20)
-        self.textLabel.move(20,0)
+#        self.textLabel.resize(280,20)
+#        self.textLabel.move(20,0)
         
         # Create textbox for config File Input
         self.textbox = QLineEdit(self)
@@ -38,20 +40,87 @@ class Ui_Form(object):
         self.button2.resize(120,30)
         self.button2.move(140,80)
         
+        # Create a button to Update Files in Cloud
+        self.button4 = QPushButton('Update File List', self)
+        self.button4.resize(120,30)
+        self.button4.move(260,60)
+        
         # Create a button to Retrieve from Cloud
         self.button3 = QPushButton('Retrieve File', self)
-        self.button3.resize(120,30)
-        self.button3.move(260,80)
+#        self.button3.resize(120,30)
+#        self.button3.move(260,80)
         
         
         # Add a Text Box Saying what file is being uploaded
         self.uploadConfirm = QLabel(self)
         self.uploadConfirm.setText("File to upload: None")
-        self.uploadConfirm.resize(280,20)
-        self.uploadConfirm.move(20,40)
+        self.uploadConfirm.setWordWrap(True)
+#        self.uploadConfirm.resize(600,40)
+#        self.uploadConfirm.move(20,40)
+        
+        
+        # Add in area with Files in Cloud
+        self.remoteFiles = QLabel(self)
+        self.remoteFiles.setText("Files in cloud: ")
+#        self.remoteFiles.resize(400,400)
+#        self.remoteFiles.move(20,50)
  
         # connect button to function on_click
         self.button.clicked.connect(self.on_click)
+        
+        self.button4.clicked.connect(self.on_click2)
+        
+        hbox = QHBoxLayout()
+        hbox.addStretch(1)
+        hbox.addWidget(self.button2)
+        hbox.addWidget(self.button3)
+        
+        hbox2 =QHBoxLayout()
+        hbox2.addWidget(self.button,1,QtCore.Qt.AlignLeft)
+        
+        
+        
+        hbox3 =QHBoxLayout()
+        hbox3.addStretch(1)
+        hbox3.addWidget(self.button4)
+        
+        hbox4 =QHBoxLayout()
+        hbox4.addWidget(self.textLabel)
+        hbox4.addWidget(self.textbox)
+        hbox6 =QHBoxLayout()
+        hbox6.addWidget(self.uploadConfirm)
+        hbox7 =QHBoxLayout()
+        hbox7.addWidget(self.remoteFiles)
+        
+       
+
+
+        vbox = QVBoxLayout()
+        vbox.addLayout(hbox4)
+        vbox.addLayout(hbox2)
+
+        
+        vbox.addLayout(hbox3)
+        
+         
+
+        vbox.addLayout(hbox6)
+
+        vbox.addLayout(hbox7)
+        vbox.addStretch(1)
+        vbox.addLayout(hbox)
+        
+        
+        
+
+        
+        
+        self.setLayout(vbox)
+         
+        
+#        self.setGeometry(300, 300, 300, 150)
+#        self.setWindowTitle('Buttons')    
+        self.show()
        
 
     def retranslateUi(self, Form):
@@ -62,11 +131,15 @@ class Ui_Form(object):
     @pyqtSlot()
     def on_click(self):
         textboxValue = self.textbox.text()
-        print(type(textboxValue))
+        if(textboxValue != ""):
+            messageToast = self.readFile(textboxValue)
+            QMessageBox.question(self, 'Message - Cloud Storage', "Your Config File is: " + messageToast, QMessageBox.Ok, QMessageBox.Ok)
+            self.textbox.setText(messageToast)
         
-        messageToast = self.readFile(textboxValue)
-        QMessageBox.question(self, 'Message - Cloud Storage', "Your Config File is: " + messageToast, QMessageBox.Ok, QMessageBox.Ok)
-        self.textbox.setText(messageToast)
+    @pyqtSlot()
+    def on_click2(self):
+        self.remoteFiles.setText("File in cloud: None")
+
  
 
 
@@ -76,8 +149,8 @@ class Widget(QtWidgets.QWidget, Ui_Form):
         self.title = 'CSE 8377 Cloud Storage'
         self.left = 10
         self.top = 10
-        self.width = 400
-        self.height = 140
+        self.width = 1200
+        self.height = 600
         self.setupUi(self)
         self.button2.clicked.connect(self.openFile)
 
